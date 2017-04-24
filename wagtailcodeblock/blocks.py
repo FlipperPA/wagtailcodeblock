528045a79c375b6143dda7fc22445f2f5c0bfee2
Allen, Timothy <tallen@wharton.upenn.edu>
2017-04-24 14:19:41 +0000

    Work in progress for line numbers and copy to clipboard.

--- wagtailcodeblock/blocks.py
+++ wagtailcodeblock/blocks.py
@@ -1,3 +1,4 @@
+from django.forms import Media
 from wagtail.wagtailcore.blocks import StructBlock, TextBlock, ChoiceBlock
 
 from .settings import get_language_choices
@@ -13,7 +14,48 @@ class CodeBlock(StructBlock):
     language = ChoiceBlock(choices=WCB_LANGUAGES)
     code = TextBlock()
 
+    @property
+    def media(self):
+        prism_version = "1.6.0"
+        js_list = [
+            "https://cdnjs.cloudflare.com/ajax/libs/prism/{}/prism.min.js".format(
+                prism_version,
+            ),
+            "https://cdnjs.cloudflare.com/ajax/libs/prism/{}/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js".format(
+                prism_version,
+            ),
+            "https://cdnjs.cloudflare.com/ajax/libs/prism/{}/plugins/line-numbers/prism-line-numbers.js".format(
+                prism_version,
+            ),
+        ]
+
+        for lang_code, lang_name in self.WCB_LANGUAGES:
+            js_list.append(
+                "https://cdnjs.cloudflare.com/ajax/libs/prism/{}/components/prism-{}.min.js".format(
+                    prism_version,
+                    lang_code,
+                )
+            )
+        print(js_list)
+        return Media(
+            js=js_list,
+            css={
+                'all': [
+                    "https://cdnjs.cloudflare.com/ajax/libs/prism/{}/themes/prism.min.css".format(
+                        prism_version,
+                    ),
+                    "https://cdnjs.cloudflare.com/ajax/libs/prism/{}/plugins/line-numbers/prism-line-numbers.min.css".format(
+                        prism_version,
+                    ),
+                    "https://cdnjs.cloudflare.com/ajax/libs/prism/{}/plugins/line-numbers/prism-line-numbers.min.css.map".format(
+                        prism_version,
+                    ),
+                ]
+            }
+        )
+
     class Meta:
         icon = 'code'
+        form_classname = 'code-block struct-block'
         template = 'wagtailcodeblock/code_block.html'
         form_template = 'wagtailcodeblock/code_block_form.html'
