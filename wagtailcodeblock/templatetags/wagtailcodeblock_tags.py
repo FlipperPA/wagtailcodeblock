a08763d8cc4d8ac589b86fb3284fa19efe28e44d
SalahAdDin <alagunasalahaddin@live.com>
2017-10-25 22:23:00 +0300

    Cleaned loaded scripts for get a correct rendering.

--- wagtailcodeblock/templatetags/wagtailcodeblock_tags.py
+++ wagtailcodeblock/templatetags/wagtailcodeblock_tags.py
@@ -9,6 +9,19 @@ from ..settings import (
 register = Library()
 
 
+def clean_list(list):
+    # Review: https://github.com/PrismJS/prism/blob/gh-pages/prism.js#L592
+    if 'html' in list:
+        list.remove('html')
+    if 'xml' in list:
+        list.remove('xml')
+    if 'mathml' in list:
+        list.remove('mathml')
+    if 'svg' in list:
+        list.remove('svg')
+    return list
+
+
 @register.simple_tag
 def load_prism_theme():
     prism_version = get_prism_version()
@@ -36,18 +49,20 @@ def load_prism_js():
 def load_prism_laguanges(field):
     prism_version = get_prism_version()
     scripts = "<script defer src='https://cdnjs.cloudflare.com/ajax/libs/prism/{0}/prism.min.js'></script>".format(
-        prism_version
+        prism_version,
     )
     values = []
     for block in field:
         if block.block_type == 'code_quote':
             values.append(block.value['code']['language'])
 
+    values = clean_list(values)
+
     for value in values:
         scripts += "" \
                    "<script defer src='https://cdnjs.cloudflare.com/ajax/libs/prism/{0}/components/prism-{1}.min.js'></script>".format(
             prism_version,
-            value
+            value,
         )
 
     return mark_safe(scripts)
