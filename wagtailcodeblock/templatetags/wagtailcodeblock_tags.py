ef7dea469ff809fbbf9be2ec3a19b160355f71a0
SalahAdDin <alagunasalahaddin@live.com>
2017-10-25 16:57:07 +0300

    Added template tag for load all scripts.

--- wagtailcodeblock/templatetags/wagtailcodeblock_tags.py
+++ wagtailcodeblock/templatetags/wagtailcodeblock_tags.py
@@ -10,12 +10,6 @@ register = Library()
 
 
 @register.simple_tag
-def get_script_version():
-    prism_version = get_prism_version()
-    return prism_version
-
-
-@register.simple_tag
 def load_prism_theme():
     prism_version = get_prism_version()
     theme = get_theme()
@@ -33,3 +27,25 @@ def load_prism_js():
         prism_version
     )
     return mark_safe(script)
+
+
+@register.simple_tag
+def load_prism_laguanges(field):
+    prism_version = get_prism_version()
+    scripts = "<script defer src='https://cdnjs.cloudflare.com/ajax/libs/prism/{0}/prism.min.js'></script>".format(
+        prism_version
+    )
+    values = []
+    for block in field:
+        if block.block_type == 'code_quote':
+            print(block.value['code']['language'])
+            values.append(block.value['code']['language'])
+
+    for value in values:
+        scripts += "" \
+                   "<script defer src='https://cdnjs.cloudflare.com/ajax/libs/prism/{0}/components/prism-{1}.min.js'></script>".format(
+            prism_version,
+            value
+        )
+
+    return mark_safe(scripts)
