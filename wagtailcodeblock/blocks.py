aa858aea9cb5352c6165badac544cfab1843290b
Allen, Timothy <tallen@wharton.upenn.edu>
2019-10-20 20:00:47 -0400

    Remove Wagtail 2.0 shim. We're 2.7 only in this version.

--- wagtailcodeblock/blocks.py
+++ wagtailcodeblock/blocks.py
@@ -1,19 +1,11 @@
 from django.forms import Media
 from django.utils.translation import ugettext_lazy as _
 
-# Wagtail 2.0 compatibility - new package paths
-try:
-    from wagtail.core.blocks import (
-        StructBlock,
-        TextBlock,
-        ChoiceBlock,
-    )
-except ImportError:
-    from wagtail.wagtailcore.blocks import (
-        StructBlock,
-        TextBlock,
-        ChoiceBlock,
-    )
+from wagtail.core.blocks import (
+    StructBlock,
+    TextBlock,
+    ChoiceBlock,
+)
 
 from .settings import (
     get_language_choices,
