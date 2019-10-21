2d844bfed89d6444b9ac5e377e71428c8be42087
Allen, Timothy <tallen@wharton.upenn.edu>
2019-10-21 12:19:59 -0400

    Fix RC.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     install_requires=[
-        'wagtail>=2.7',
+        'wagtail>=2.7rc1',
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
