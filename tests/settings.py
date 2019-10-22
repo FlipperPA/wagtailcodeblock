9dba0a89cf44f3ed35f82f352a09a45fbf08b22f
Allen, Timothy <tallen@wharton.upenn.edu>
2019-10-22 10:48:15 -0400

    More test settings to allow running a minimal app with manage.py for development in addition to testing.

--- tests/settings.py
+++ tests/settings.py
@@ -1,15 +1,30 @@
 from django.conf import settings
 
 SECRET_KEY = "tests"
+DEBUG = True
 
 TEMPLATES = [
     {"BACKEND": "django.template.backends.django.DjangoTemplates", "APP_DIRS": True}
 ]
 
 INSTALLED_APPS = settings.INSTALLED_APPS + [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+
     "wagtail.core",
     "wagtail.admin",
     "wagtailcodeblock",
+
+    "tests",
 ]
 
 MIDDLEWARE = settings.MIDDLEWARE + ["wagtail.core.middleware.SiteMiddleware"]
+
+ROOT_URLCONF = "tests.urls"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test_db.sqlite3',
+    }
+}
