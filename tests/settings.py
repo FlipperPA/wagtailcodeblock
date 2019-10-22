672d494de00ada2be9a34d7873a04b38d192ce44
Allen, Timothy <tallen@wharton.upenn.edu>
2019-10-22 17:23:52 -0400

    Get working test site up and running.

--- tests/settings.py
+++ tests/settings.py
@@ -4,27 +4,54 @@ SECRET_KEY = "tests"
 DEBUG = True
 
 TEMPLATES = [
-    {"BACKEND": "django.template.backends.django.DjangoTemplates", "APP_DIRS": True}
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
+        },
+    }
 ]
 
 INSTALLED_APPS = settings.INSTALLED_APPS + [
     "django.contrib.auth",
     "django.contrib.contenttypes",
-
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
     "wagtail.core",
     "wagtail.admin",
+    "wagtail.documents",
+    "wagtail.images",
+    "wagtail.users",
     "wagtailcodeblock",
-
+    "modelcluster",
+    "taggit",
     "tests",
 ]
 
-MIDDLEWARE = settings.MIDDLEWARE + ["wagtail.core.middleware.SiteMiddleware"]
+MIDDLEWARE = settings.MIDDLEWARE + [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "wagtail.core.middleware.SiteMiddleware",
+]
 
 ROOT_URLCONF = "tests.urls"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'test_db.sqlite3',
-    }
+    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "test_db.sqlite3"}
 }
+
+STATIC_URL = "/static/"
+
+WAGTAIL_SITE_NAME = "Test Site"
--- tests/urls.py
+++ tests/urls.py
@@ -1,7 +1,11 @@
 from django.urls import include, re_path
 
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
 
 urlpatterns = [
+    re_path(r'^cms/', include(wagtailadmin_urls)),
+    re_path(r'^documents/', include(wagtaildocs_urls)),
     re_path(r'', include(wagtail_urls)),
 ]
