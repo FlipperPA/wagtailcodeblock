abc7b760d691dbf9862dba4c931638302151ce41
Allen, Timothy <tallen@wharton.upenn.edu>
2019-10-23 16:47:25 -0400

    WIP: come up with decent tests for StreamBlocks.

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -11,8 +11,10 @@ def test_create_page():
     Tests creating a page with a Code Block.
     """
     t = CodeBlockPage(
-        title='Test Page',
-        slug='test-page',
+        title="Test Page",
+        slug="test-page",
+        path="000100010001",
+        depth=3,
         body=dumps([{
             "type": "code",
             "value": {
@@ -23,13 +25,13 @@ def test_create_page():
     )
     t.save()
 
-    assert True
-
-
-"""
-def test_render_moment_unlocalized():
-    form = forms.DateTimeFieldForm()
-    widget = form.fields["datetime_field"].widget
-    assert isinstance(widget, widgets.DateTimePicker)
-    assert "YYYY-MM-DD HH:mm:ss" in widget.js_options["format"]
-"""
+    assert t.body == """<div class="block-code">
+<script src="/static/wagtailcodeblock/js/prism.min.js" type='text/javascript'></script><link href="/static/wagtailcodeblock/css/prism.min.css" rel="stylesheet"><link href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism-coy.min.css' rel='stylesheet'/><script>
+                language_class_name = 'language-python';
+            </script><pre class="line-numbers"><code id="target-element-current">print([x for x in range(1, 5)])</code></pre><script>
+                var block_num = (typeof block_num === 'undefined') ? 0 : block_num;
+                block_num++;
+                document.getElementById('target-element-current').className = language_class_name;
+                document.getElementById('target-element-current').id = 'target-element-' + block_num;
+            </script>
+</div>"""
