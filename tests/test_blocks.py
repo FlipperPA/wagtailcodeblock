2981016c66c8e82b506049ae2cab73a895b38182
Allen, Timothy <tallen@wharton.upenn.edu>
2019-10-24 07:49:54 -0400

    pytest.ini is now working with configuration end-to-end. Now to get more coverage...

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -25,13 +25,5 @@ def test_create_page():
     )
     t.save()
 
-    assert t.body == """<div class="block-code">
-<script src="/static/wagtailcodeblock/js/prism.min.js" type='text/javascript'></script><link href="/static/wagtailcodeblock/css/prism.min.css" rel="stylesheet"><link href='https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism-coy.min.css' rel='stylesheet'/><script>
-                language_class_name = 'language-python';
-            </script><pre class="line-numbers"><code id="target-element-current">print([x for x in range(1, 5)])</code></pre><script>
-                var block_num = (typeof block_num === 'undefined') ? 0 : block_num;
-                block_num++;
-                document.getElementById('target-element-current').className = language_class_name;
-                document.getElementById('target-element-current').id = 'target-element-' + block_num;
-            </script>
-</div>"""
+    assert '<code id="target-element-current">print([x for x in range(1, 5)])</code>' \
+    	in t.body.render_as_block()
