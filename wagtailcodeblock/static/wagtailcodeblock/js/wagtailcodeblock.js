f5cad5cbf6e7d94a25398c3da366ddd2e1a4f470
Arturs Ontuzans <Arturinsh@users.noreply.github.com>
2023-02-16 14:45:12 +0000

    Wagtail 4.2 admin streamfield TypeError fix

--- wagtailcodeblock/static/wagtailcodeblock/js/wagtailcodeblock.js
+++ wagtailcodeblock/static/wagtailcodeblock/js/wagtailcodeblock.js
@@ -1,38 +1,40 @@
-function CodeBlockDefinition() {
-    window.wagtailStreamField.blocks.StructBlockDefinition.apply(this, arguments);
-}
+class CodeBlockDefinition extends window.wagtailStreamField.blocks
+    .StructBlockDefinition {
+    render(placeholder, prefix, initialState, initialError) {
+        const block = super.render(
+            placeholder,
+            prefix,
+            initialState,
+            initialError,
+        );
 
-CodeBlockDefinition.prototype.render = function(placeholder, prefix, initialState, initialError) {
-    var block = window.wagtailStreamField.blocks.StructBlockDefinition.prototype.render.apply(
-        this, arguments
-    );
+        var languageField = $(document).find('#' + prefix + '-language');
+        var codeField = $(document).find('#' + prefix + '-code');
+        var targetField = $(document).find('#' + prefix + '-target');
 
-    var languageField = $(document).find('#' + prefix + '-language');
-    var codeField = $(document).find('#' + prefix + '-code');
-    var targetField = $(document).find('#' + prefix + '-target');
+        function updateLanguage() {
+            var languageCode = languageField.val();
+            targetField.removeClass().addClass('language-' + languageCode);
+            prismRepaint();
+        }
 
-    function updateLanguage() {
-        var languageCode = languageField.val();
-        targetField.removeClass().addClass('language-' + languageCode);
-        prismRepaint();
+        function prismRepaint() {
+            Prism.highlightElement(targetField[0]);
+        }
+
+        function populateTargetCode() {
+            var codeText = codeField.val();
+            targetField.text(codeText);
+            prismRepaint(targetField);
+        }
+
+        updateLanguage();
+        populateTargetCode();
+        languageField.on('change', updateLanguage);
+        codeField.on('keyup', populateTargetCode);
+
+        return block;
     }
-
-    function prismRepaint() {
-        Prism.highlightElement(targetField[0]);
-    }
-
-    function populateTargetCode() {
-        var codeText = codeField.val();
-        targetField.text(codeText);
-        prismRepaint(targetField);
-    }
-
-    updateLanguage();
-    populateTargetCode();
-    languageField.on('change', updateLanguage);
-    codeField.on('keyup', populateTargetCode);
-
-    return block;
 }
 
 window.telepath.register('wagtailcodeblock.blocks.CodeBlock', CodeBlockDefinition);
